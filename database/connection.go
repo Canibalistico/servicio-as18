6da20f742fee6c4c8529757ed401aadf3b6c118d
Canibalistico <119649445+Canibalistico@users.noreply.github.com>
2023-10-17 10:57:15 -0300

    Update connection.go

--- database/connection.go
+++ database/connection.go
@@ -7,8 +7,7 @@ import (
 	"gorm.io/gorm"
 )
 
-// var DSN = "root:@tcp(127.0.0.1:3306)/meetings?charset=utf8mb4&parseTime=True&loc=Local"
-var DSN = "cra7232_rapidweb:J@ck1984@tcp(190.107.177.234:3306)/cra7232_mytext?charset=utf8mb4&parseTime=True"
+var DSN = "root:@tcp(127.0.0.1:3306)/meetings?charset=utf8mb4&parseTime=True&loc=Local"
 var DB *gorm.DB
 
 func DBconnection() {
